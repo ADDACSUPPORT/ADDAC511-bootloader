095a538229511da32719a16ff8129df8a7dc6869
tfps <tfps.addac@gmail.com>
2026-04-07 14:46:50 +0100

    modified text

--- app/app.js
+++ app/app.js
@@ -188,7 +188,7 @@ var app = new Vue({
                     <li><p>Press 'Connect' button above. A pop-up window with a list of usb devices will appear</p></li>
                     <li><p>Select 'DFU in FS mode - Paired' and press 'Connect'</p></li>
                     <li><p>Press "Flash Bootloader Image". A progress bar will appear</p></li>
-                    <li><p>Once finished disconnect the USB cable and power OFF the module</p></li>
+                    <li><p>Once finished disconnect the USB cable and restart the module</p></li>
                 <p>
                     On windows, you may have to update the driver to WinUSB.
 
